9c7c9e7ef9cdfd02be2e9cb326846cf7f62d8253
Pawcio49 <pawel.dopierala4@gmail.com>
2023-05-21 17:39:33 +0200

    Generowanie subsetów

--- genkomb2.c
+++ genkomb2.c
@@ -6,34 +6,30 @@
     #include <malloc.h>
     #include <omp.h>
      
-    typedef struct REC {
-        char v;
-        struct REC * next0;
-        struct REC * next1;
-    } rec;
+    typedef struct REC { char v; struct REC * prev; } rec;
      
-    // void drukuj (rec * x, int n) {
-    //  char * buf = malloc(sizeof(char)*(n+1));
-    //  buf[n]=0; while (x) {buf[--n]=x->v; x = x -> prev;}
-    //  printf("%d: %s\n",omp_get_thread_num(),buf);
-    //  free(buf);
-    // }
+    void drukuj (rec * x, int n) {
+     char * buf = malloc(sizeof(char)*(n+1));
+     buf[n]=0; while (x) {buf[--n]=x->v; x = x -> prev;}
+     printf("%d: %s\n",omp_get_thread_num(),buf);
+     free(buf);
+    }
      
     void genkomb(rec * x, int level, int n, int k) {
      rec X1,X2;
-     if (level==n) {} 
+     if (level==n) {drukuj(x,n);} 
      else 
       {
         #pragma omp task
-        {
-            x->next0 = &X1;
-            X1.v = '0';
+        { 
+          X1.prev = x;
+          X1.v = '0';
           if (n-level>k) genkomb(&X1,level+1,n,k);
         }
         #pragma omp task  
         {
-            x->next1 = &X2;
-            X2.v = '1';
+          X2.prev = x;
+          X2.v = '1';
          if (k>0) genkomb(&X2,level+1,n,k-1);
         }
         #pragma omp taskwait  
@@ -43,14 +39,12 @@
     int main(int argc, char * argv[]) {
      int n = 1;
      int k = 1;
-     rec x;
-     x.v = -1;
+     
      if (argc > 1)  n = atoi(argv[1]);
      if (argc > 2)  k = atoi(argv[2]);
      #pragma omp parallel
      #pragma omp single
-     genkomb(&x,0,n,k);
-
+     genkomb(NULL,0,n,k);
      return EXIT_SUCCESS;
     }
 
